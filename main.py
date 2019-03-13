2239ed1903aa01f8a3889089c53a03c97890f9a4
betaBison <knowles.derek@gmail.com>
2019-03-12 18:29:42 -0600

    notifiers

--- main.py
+++ main.py
@@ -8,30 +8,37 @@ from motorControl import MotorControl
 
 def main():
     motors = MotorControl()
+    motors.disable()
 
+    print("starting cw")
     for ii in range(100):
         throttle = ii/100.0
         motors.cw(throttle)
         time.sleep(0.1)
     motors.disable()
 
+    print("starting ccw")
     for ii in range(100):
         throttle = ii/100.0
         motors.ccw(throttle)
         time.sleep(0.1)
     motors.disable()
 
+    print("starting forward")
     for ii in range(100):
         throttle = ii/100.0
         motors.forward(throttle)
         time.sleep(0.1)
     motors.disable()
 
+    print("starting backward")
     for ii in range(100):
         throttle = ii/100.0
         motors.backward(throttle)
         time.sleep(0.1)
     motors.disable()
+
+    print("end of script")
 
 
 

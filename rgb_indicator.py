35fe148a2474250439644f1bededfbe0146ac1f4
betaBison <knowles.derek@gmail.com>
2019-03-21 22:54:47 +0000

    some blinking

--- rgb_indicator.py
+++ rgb_indicator.py
@@ -4,6 +4,7 @@ Desc  : generalized PID control
 """
 import RPi.GPIO as GPIO
 import time
+import numpy as np
 
 class RGB():
 	def __init__(self):
@@ -12,36 +13,38 @@ class RGB():
 		self.SDI = 37
 		self.RCLK = 38
 		self.SRCLK = 40
-
+		
+		GPIO.setwarnings(False)	#don't show setup warnings
 		GPIO.setup(self.SDI, GPIO.OUT, initial=GPIO.LOW)
 		GPIO.setup(self.RCLK, GPIO.OUT, initial=GPIO.LOW)
 		GPIO.setup(self.SRCLK, GPIO.OUT, initial=GPIO.LOW)
 
 		self.off = np.zeros((24,1))
-		self.on = np.ones((24,1))
+		self.on = [0xFFFFFF,0x0,0xFFFFFF,0x0,0xFFFFFF,0x0]
 		self.LED0 = [0x01,0x02,0x04,0x08,0x10,0x20,0x40,0x80]	#original mode
 		self.LED1 = [0x01,0x03,0x07,0x0f,0x1f,0x3f,0x7f,0xff]	#blink mode 1
 		self.LED2 = [0x01,0x05,0x15,0x55,0xb5,0xf5,0xfb,0xff]	#blink mode 2
 		self.LED3 = [0x02,0x03,0x0b,0x0f,0x2f,0x3f,0xbf,0xff]	#blink mode 3
+		self.LED4 = [0xE00000,0x0,0x1C0000,0x0,0x38000,0x0,0x7000,0x0]
 
-	def print_msg():
+	def print_msg(self):
 		print('Program is running...')
 		print('Please press Ctrl+C to end the program...')
 
-	def hc595_in(dat):
-		for bit in range(0, 8):
-			GPIO.output(self.SDI, 0x80 & (dat << bit))
+	def hc595_in(self,dat):
+		for bit in range(0, 24):
+			GPIO.output(self.SDI, 0x800000 & (dat << bit))
 			GPIO.output(self.SRCLK, GPIO.HIGH)
 			time.sleep(0.001)
 			GPIO.output(self.SRCLK, GPIO.LOW)
 
-	def hc595_out():
+	def hc595_out(self):
 		GPIO.output(self.RCLK, GPIO.HIGH)
 		time.sleep(0.001)
 		GPIO.output(self.RCLK, GPIO.LOW)
 
-	def loop():
-		WhichLeds = self.LED0	# Change Mode, modes from LED0 to LED3
+	def loop(self):
+		WhichLeds = self.on	# Change Mode, modes from LED0 to LED3
 		sleeptime = 0.1		# Change speed, lower value, faster speed
 		while True:
 			for i in range(0, len(WhichLeds)):
@@ -49,12 +52,7 @@ class RGB():
 				self.hc595_out()
 				time.sleep(sleeptime)
 
-			for i in range(len(WhichLeds)-1, -1, -1):
-				self.hc595_in(WhichLeds[i])
-				self.hc595_out()
-				time.sleep(sleeptime)
-
-	def destroy():   # When program ending, the function is executed.
+	def destroy(self):   # When program ending, the function is executed.
 		GPIO.cleanup()
 
 if __name__ == '__main__':

1d5dd3396e2d1f796096242425f0194b5c9d48ad
betaBison <knowles.derek@gmail.com>
2019-04-18 13:45:49 -0600

    pin changes

--- src/umnitsa_rpi/src/param_RPiPins.py
+++ src/umnitsa_rpi/src/param_RPiPins.py
@@ -1,10 +1,10 @@
 # motor control pins
-DB1 = 3                     # Driver Board #1 INH
-M1 = 5                      # DB #1 IN1 & IN2
-M2 = 7                      # DB #1 IN3 & IN4
-DB2 = 8                     # DB #2 INH
-M3 = 10                     # DB #2 IN1 & IN2
-M4 = 11                     # DB #2 IN3 & IN4
+DB1 = 7                     # Driver Board #1 INH
+M1 = 8                      # DB #1 IN1 & IN2
+M2 = 10                      # DB #1 IN3 & IN4
+DB2 = 11                     # DB #2 INH
+M3 = 12                     # DB #2 IN1 & IN2
+M4 = 13                     # DB #2 IN3 & IN4
 
 # ultrasonic pins
 ULTRA1_TRIG = 18
@@ -17,6 +17,11 @@ ULTRA4_TRIG = 26
 ULTRA4_ECHO = 29
 
 # rgb pins
+BUZ = 36
 RGB_SDI = 37
 RGB_RCLK = 38
 RGB_SRCLK = 40
+
+# Magnetometer
+SDA = 3
+SCL = 5
